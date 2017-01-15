4b0990448bcbd3d0251381d510a5d2a01217f3f6
Bastien Thomas <bastien.thomas@happn.com>
2017-01-15 02:59:10 +0100

    Badge icon color

--- js/background.js
+++ js/background.js
@@ -2,6 +2,9 @@ chrome.browserAction.setBadgeBackgroundColor({
     color: [190, 190, 190, 230]
 });
 
+chrome.browserAction.setBadgeBackgroundColor({color: "black"});
+
+
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
         switch (message.type) {
